3aaf91e377c7eb5e212815ce301e7ab300875eb1
Giuliano Di Pasquale <dipasqualegiuliano@gmail.com>
2018-12-29 00:18:39 +0100

    Add Batch Input via "batch" files

--- main.go
+++ main.go
@@ -1,19 +1,47 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	"github.com/Nooby/Synacor-Challenge/term"
 	"github.com/Nooby/Synacor-Challenge/vm"
 )
 
-func main() {
-	challengeBinaryPath := "challenge.bin"
+const (
+	defaultChallengePath = "challenge.bin"
+	challengePathUsage   = "path to challenge binary"
+	commandInputUsage    = "path to batch commands file"
+)
 
-	in, out := term.HandleConsole()
+var challengeBin string
+var commandInput string
+
+func init() {
+	flag.StringVar(&challengeBin, "bin",
+		defaultChallengePath, challengePathUsage)
+	flag.StringVar(&challengeBin, "b",
+		defaultChallengePath, challengePathUsage+" (shorthand)")
+	flag.StringVar(&commandInput, "input", "", commandInputUsage)
+	flag.StringVar(&commandInput, "i", "", commandInputUsage+" (shorthand)")
+}
+
+func main() {
+	flag.Parse()
+
+	in := make(chan uint16)
+	out := make(chan uint16)
+
+	if len(commandInput) > 0 {
+		go term.HandleBatchInput(in, commandInput)
+	} else {
+		go term.HandleConsoleInput(in)
+	}
+
+	go term.HandleOutput(out)
 
 	cpu := vm.NewCPU(in, out)
-	err := cpu.LoadImage(challengeBinaryPath)
+	err := cpu.LoadImage(challengeBin)
 	if err != nil {
 		fmt.Println(err)
 		return
--- term/term.go
+++ term/term.go
@@ -1,41 +1,54 @@
 package term
 
 import (
-	"os"
-	"fmt"
 	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
 )
 
-func HandleConsole() (consoleInput, consoleOutput chan uint16) {
-	consoleInput = make(chan uint16)
-	consoleOutput = make(chan uint16)
-
-	go HandleIn(consoleInput)
-	go HandleOut(consoleOutput)
-
-	return consoleInput, consoleOutput
-}
-
-func HandleIn(in chan uint16) {
-	var text string
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		text = scanner.Text()
-		for ic := 0; ic < len(text); ic++ {
-			value := text[ic]
-			if value >= 128 {
-				panic("Only ASCII Runes Supported.")
-			}
-			in<- uint16(value)
-		}
-		in<- 10	// Scanner removes the Line Feed; Re Adding it.
+// HandleBatchInput reads the content of the file at 'path' to the chan 'in'.
+// After EOF of the file HandleBatchInput reads Stdin to the chan.
+// Lines starting with '#' and empty lines will be ignored.
+func HandleBatchInput(in chan uint16, path string) {
+	f, err := os.Open(path)
+	if err != nil {
+		log.Fatalf("Unable to open Batch Input: %v", err)
 	}
+	readTo(in, f)
+	readTo(in, os.Stdin)
 }
 
-func HandleOut(out chan uint16) {
+// HandleConsoleInput reads Stdin to the chanel 'in'.
+// Lines starting with '#' and empty lines will be ignored.
+func HandleConsoleInput(in chan uint16) {
+	readTo(in, os.Stdin)
+}
+
+// HandleOutput writes chan 'out' to Stdout.
+func HandleOutput(out chan uint16) {
 	var text string
 	for c := range out {
 		text = string(c)
 		fmt.Print(text)
+	}
+}
+
+func readTo(in chan uint16, reader io.Reader) {
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() {
+		text := scanner.Text()
+		if len(text) <= 0 || text[0] == '#' {
+			continue
+		}
+		for ic := 0; ic < len(text); ic++ {
+			value := text[ic]
+			if value >= 128 {
+				log.Fatal("Only ASCII Runes Supported.")
+			}
+			in <- uint16(value)
+		}
+		in <- 10 // Scanner removes the Line Feed; Re Adding it.
 	}
 }
