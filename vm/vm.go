5f57ba0ca6e41b0972f787fffcc5ec9cf7ac8262
Giuliano Di Pasquale <dipasqualegiuliano@gmail.com>
2018-12-29 00:34:03 +0100

    Cleanup and Comments

--- vm/vm.go
+++ vm/vm.go
@@ -1,30 +1,34 @@
-// Package VM implements the Execution and Memory Model of the Synacor VM.
+// Package vm implements the Execution and Memory Model of the Synacor VM.
 package vm
 
 import (
-	"os"
 	"encoding/binary"
+	"log"
+	"os"
 )
 
 const (
-	addressSpace uint16 = 32768
+	addressSpace     uint16 = 32768
 	validNumberRange uint16 = 32776
 )
 
+// CPU represents the CPU that executes the instructions loaded into its memory.
 type CPU struct {
-	cursor uint16
-	consoleIn chan uint16
+	cursor     uint16
+	consoleIn  chan uint16
 	consoleOut chan uint16
-	Memory []uint16
-	Registers [8]uint16
-	Stack []uint16
+	Memory     []uint16
+	Registers  [8]uint16
+	Stack      []uint16
 }
 
+// NewCPU constructs a new CPU.
 func NewCPU(in chan uint16, out chan uint16) *CPU {
-    c := CPU{consoleIn: in, consoleOut: out}
-    return &c
+	c := CPU{consoleIn: in, consoleOut: out}
+	return &c
 }
 
+// LoadImage loads a binary file into the main Memory of the CPU.
 func (cpu *CPU) LoadImage(path string) error {
 	file, err := os.Open(path)
 	if err != nil {
@@ -51,43 +55,65 @@ func (cpu *CPU) LoadImage(path string) error {
 	return nil
 }
 
+// Execute reads operations from the main Memory and interpretes them until the function
+// encounters the Halt OP or a Fatal Error occurs.
 func (cpu *CPU) Execute() {
 	for {
 		switch code := cpu.read(); code {
-			case halt: cpu.halt()
-			case set: cpu.set()
-			case push: cpu.push()
-			case pop: cpu.pop()
-			case eq: cpu.eq()
-			case gt: cpu.gt()
-			case jmp: cpu.jmp()
-			case jt: cpu.jt()
-			case jf: cpu.jf()
-			case add: cpu.add()
-			case mult: cpu.mult()
-			case mod: cpu.mod()
-			case and: cpu.and()
-			case or: cpu.or()
-			case not: cpu.not()
-			case rmem: cpu.rmem()
-			case wmem: cpu.wmem()
-			case call: cpu.call()
-			case ret: cpu.ret()
-			case out: cpu.out()
-			case in: cpu.in()
-			case noop: // Nothing to do.
-			default:
-				panic("Unknown OpCode")
-				return
+		case halt:
+			cpu.halt()
+		case set:
+			cpu.set()
+		case push:
+			cpu.push()
+		case pop:
+			cpu.pop()
+		case eq:
+			cpu.eq()
+		case gt:
+			cpu.gt()
+		case jmp:
+			cpu.jmp()
+		case jt:
+			cpu.jt()
+		case jf:
+			cpu.jf()
+		case add:
+			cpu.add()
+		case mult:
+			cpu.mult()
+		case mod:
+			cpu.mod()
+		case and:
+			cpu.and()
+		case or:
+			cpu.or()
+		case not:
+			cpu.not()
+		case rmem:
+			cpu.rmem()
+		case wmem:
+			cpu.wmem()
+		case call:
+			cpu.call()
+		case ret:
+			cpu.ret()
+		case out:
+			cpu.out()
+		case in:
+			cpu.in()
+		case noop: // Nothing to do.
+		default:
+			log.Fatal("Unknown OpCode")
 		}
 	}
 }
 
 func (cpu *CPU) read() uint16 {
 	element := cpu.Memory[cpu.cursor]
-	cpu.cursor += 1
+	cpu.cursor++
 	if element >= validNumberRange {
-		panic("Value outside of Integer Range.")
+		log.Fatal("Value outside of Integer Range.")
 	}
 	return element
 }
@@ -103,11 +129,11 @@ func (cpu *CPU) readAsValue() uint16 {
 
 func (cpu *CPU) readAsRegister() uint16 {
 	element := cpu.read()
-	if element >= addressSpace {
-		register := element - addressSpace
-		return register
+	if element < addressSpace {
+		log.Fatal(string(element) + " is not a Register.")
 	}
-	panic(string(element) + " is not a Register.")
+	register := element - addressSpace
+	return register
 }
 
 // imageSize calculates how many uint16 values need to be stored in VM Memory
